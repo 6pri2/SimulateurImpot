be6d4dc9cb3a64c5878e30f34011f6e938bc9dcf
Cyprien <cyprien.duroy@etu.unicaen.fr>
2025-04-29 19:17:37 +0200

    Création class de validation

---
+++ src/main/java/com/kerware/simulateur/util/ValidationUtil.java
@@ -0,0 +1,13 @@
+package com.kerware.simulateur.util;
+
+public final class ValidationUtil {
+    private ValidationUtil() {}
+
+    public static void checkNonNegative(int value, String message) {
+        if (value < 0) throw new IllegalArgumentException(message);
+    }
+
+    public static void checkNotNull(Object obj, String message) {
+        if (obj == null) throw new IllegalArgumentException(message);
+    }
+}
